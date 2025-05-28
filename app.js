59587afee4638709fb6b87d4b0d6aae0e148dc4b
AmandaJCampos <amandajcampos8@gmail.com>
2025-05-28 15:53:06 -0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
-import logger from "./middleware/logger.js"
+import logger from "./middleware/logger.js";
+import cors from 'cors';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(logger);
+app.use(cors());
 
 // Rotas
 app.use("/api/users", userRoutes);
